031ac8a783e81b26e8277a7445deb30e4504c752
bouda04 <bouda04@gmail.com>
2017-09-20 18:02:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bouda04/ex16b/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bouda04.ex16b;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
