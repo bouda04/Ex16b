3c6bca8b8fad9ac472fffd748c27722f1ac0545b
bouda04 <bouda04@gmail.com>
2017-09-20 18:19:37 +0300

    cleanup and reorganize the layout

--- app/src/main/java/com/example/bouda04/ex16b/MainActivity.java
+++ app/src/main/java/com/example/bouda04/ex16b/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.bouda04.ex16b;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
+    private Object goLock = new Object();
+    private Object syncher = new Object();
+    private boolean go = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startThem(View v){
+
+        synchronized (goLock) {
+            go = true;
+            goLock.notifyAll();
+        }
     }
 }
